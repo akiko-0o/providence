5f8140117abc56b7188b5e4260dfa0fcbc14aa47
akiko-0o <aki0402.0o@gmail.com>
2020-07-09 15:51:06 +0900

    firstcommit

---
+++ assets/js/common.js
@@ -0,0 +1,17 @@
+
+$(function(){
+    $("li").hover(function(){
+        $(this).append("<div><p>" + $(this)
+        .children("a").children("img").attr("alt") + "</p></div>");
+
+        $(this).children("div").stop().fadeIn(300);
+        $(this).children("div").children("p").stop().
+        animate({"top" : 0}, 300);
+    },function(){
+        $(this).children("div").stop().fadeOut(300);
+        $(this).children("div").children("p").stop().
+        animate(300, function(){
+            $(this).parent("div").remove();
+        });
+    });
+});
