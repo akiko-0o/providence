df2b889e323eb6dfbbc5a72fe16d1061fecf6c25
akiko-0o <aki0402.0o@gmail.com>
2020-07-15 14:36:38 +0900

    スライドショー実装

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,14 @@
 
-$(function(){
-    $("li").hover(function(){
-        $(this).append("<div><p>" + $(this)
-        .children("a").children("img").attr("alt") + "</p></div>");
 
+$(function(){
+    $(".works__list").hover(function(){
+        $(this).append('<div class="link"><p>'+ $(this)
+        .children("a").children("img").attr("alt") + "</p></div>");
+        $("a").addClass("changed");
         $(this).children("div").stop().fadeIn(300);
         $(this).children("div").children("p").stop().
         animate({"top" : 0}, 300);
+
     },function(){
         $(this).children("div").stop().fadeOut(300);
         $(this).children("div").children("p").stop().
@@ -14,4 +16,10 @@ $(function(){
             $(this).parent("div").remove();
         });
     });
+    $(".link").click(function(){
+        $("changed").click();
+      });
+
+      
+    
 });
